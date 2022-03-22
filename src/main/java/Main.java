71c99c0602502d318fca1b5d73ad78152bc1e97b
Luis Mi <luismiguelbravolopez3@gmail.com>
2022-03-22 10:30:22 +0100

    Commit Adivina Palabra

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,8 +4,18 @@ public class Main {
     public static void main(String[] args){
         System.out.print("Proyecto Listo para GitHub");
         Scanner sc = new Scanner(System.in);
-        String texto = sc.nextLine();
-        System.out.print("He recibido" + texto);
+        String texto = "";
+        String pass = "1234";
+
+
+        do{
+            System.out.print("Adivina mi contraseña secreta:");
+            texto = sc.nextLine();
+            System.out.print("He recibido" + texto);
+        }while(texto != pass);
+
+
+
     }
 
 
