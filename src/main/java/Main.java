553a2784f687be42056e693434422d86fcf114c6
Luis Mi <luismiguelbravolopez3@gmail.com>
2022-03-22 10:25:34 +0100

    Pedir

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,11 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
         System.out.print("Proyecto Listo para GitHub");
-
-
+        Scanner sc = new Scanner(System.in);
+        String texto = sc.nextLine();
+        System.out.print("He recibido" + texto);
     }
 
 
