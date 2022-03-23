a7c268b6e18dfc20e0909f3c53614bd12334651e
Luis Mi <luismiguelbravolopez3@gmail.com>
2022-03-23 10:09:39 +0100

    Commit FakeInput 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,17 +5,17 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
+        //Scanner sc = new Scanner(System.in);
         String texto = "";
         String pass = "1234";
-        String fakeInput = "1111";
+        String fakeInput = "1111\n2222"; //\n1234";
         InputStream inputStream = new ByteArrayInputStream(fakeInput.getBytes());
-
+        Scanner sc = new Scanner(inputStream);
         do{
             System.out.println("Adivina mi contraseña secreta:");
             texto = sc.nextLine();
             System.out.print("He recibido" + texto);
-        }while(!texto.equals(pass));
+        }while(!texto.equals(pass) && sc.hasNext());
 
 
 
