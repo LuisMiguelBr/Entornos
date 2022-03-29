e4467fa2776cad6fdf3c4baccb8eccac5ce5ddc5
LuisMiguelBr <luismiguelbravolopez3@gmail.com>
2022-03-29 10:00:45 +0200

    Ejercicio

--- src/main/java/Clase2.java
+++ src/main/java/Clase2.java
@@ -1,9 +1,20 @@
 public class Clase2 {
     public static void main(String [] args){
         Usuario u1 = new Usuario("MechaCristo","1234");
-        //Usuario u2 = new Usuario();
-        System.out.println(u1);
+        Usuario u2 = new Usuario();
+        System.out.println(u2);
+        Usuario[] usuarios = new Usuario[2];
+        usuarios[0] = u1;
+        usuarios[1] = u2;
 
+        // Pedir inciar sesion a un usuario
+        // Primero hay que comprobar el nombre
+        // Si el nombre es correcto entonces hay que pedir la contraseña
+        // Si no te sigo preguntando por el nombre.
+        // Si la contraseña es correcta te digo, usuario logeado!
+        // Si no te pregunto hasta 2 veces por la contraseña como maximo
+        // Si tras esos intentos no he conseguido acertar, entonces finalizara el programa diciendo
+        // Alerta Hackerman!
     }
 
 
